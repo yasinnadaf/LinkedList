ec4c9f9225c042ae943e7a09275aa63a0f0c8f83
yasin nadaf <yasin.nadaf7414@gmai.com>
2022-07-26 23:24:56 +0530

    Added: program to print a simple linked list

---
+++ src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedListMain {
+    
+    public static void main(String[] args) {
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.push(70);
+        linkedList.push(30);
+        linkedList.push(56);
+
+        linkedList.print();
+    }
+
+}
+
